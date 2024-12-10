cb9248fb95818f415d75dd6adc95310ae75db47b
VenkataJayanth9 <143101611+VenkataJayanth-9@users.noreply.github.com>
2024-12-10 17:06:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Non-overlapping Intervals/nonoverlapping-intervals.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int minRemoval(vector<vector<int>> &intervals) {
+        // code here
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];
+    });
+
+    int count = 0; // Count of removed intervals
+    int prev_end = INT_MIN; // The end of the last non-overlapping interval
+
+    for (const auto& interval : intervals) {
+        if (interval[0] >= prev_end) {
+            // If the current interval does not overlap
+            prev_end = interval[1];
+        } else {
+            // If it overlaps, increment the removal count
+            count++;
+        }
+    }
+
+    return count;
+
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        vector<vector<int>> intervals(N, vector<int>(2));
+        for (int i = 0; i < N; i++) {
+            cin >> intervals[i][0] >> intervals[i][1];
+        }
+        Solution obj;
+        cout << obj.minRemoval(intervals) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
