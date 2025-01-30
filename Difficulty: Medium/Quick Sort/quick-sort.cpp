7366cbf35aa7724cc7e9ac60dc7b2c87d4a3dc4d
VenkataJayanth9 <143101611+VenkataJayanth-9@users.noreply.github.com>
2025-01-30 09:41:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to print an array */
+void printArray(const vector<int>& arr) {
+    for (int num : arr)
+        printf("%d ", num);
+    printf("\n");
+}
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void quickSort(vector<int>& arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low - 1, j = high + 1;
+
+        while (true) {
+            do {
+                i++;
+            } while (arr[i] < pivot);
+
+            do {
+                j--;
+            } while (arr[j] > pivot);
+
+            if (i >= j)
+                return j;
+
+            swap(arr[i], arr[j]);
+        }
+    }
+};
+
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    scanf("%d", &T);
+    getchar(); // to consume newline after T
+
+    while (T--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.quickSort(arr, 0, arr.size() - 1);
+        printArray(arr);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
