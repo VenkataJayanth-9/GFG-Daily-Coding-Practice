6455feb494f6f78e8c00df7c6888f2e2893b4dd8
VenkataJayanth9 <143101611+VenkataJayanth-9@users.noreply.github.com>
2025-01-31 16:37:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Wave Array/wave-array.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <algorithm>
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // arr: input array
+    // Function to sort the array into a wave-like array.
+    void convertToWave(vector<int>& arr) {
+        // code here
+        int size = arr.size();
+        
+        for(int i = 0; i<size; i+=2){
+            if(size % 2 != 0 && i == size-1){
+                break;
+            }
+            else{
+                int j = i+1;
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t, n;
+    cin >> t; // Input testcases
+    cin.ignore();
+    while (t--) // While testcases exist
+    {
+        vector<int> a;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        sort(a.begin(), a.end());
+        Solution ob;
+        ob.convertToWave(a);
+
+        for (int i = 0; i < a.size(); i++)
+            cout << a[i] << " ";
+
+        cout << endl;
+        cout << "~" << endl;
+    }
+}
+// } Driver Code Ends
